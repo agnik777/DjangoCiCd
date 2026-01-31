ab9680505826e57245a36fc2fd3363fcd1dbf447
Andrey Nikolaev <agnik@europe.com>
2026-01-31 15:40:03 +0300

    first_commit

---
+++ logistic/views.py
@@ -0,0 +1,34 @@
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.decorators import api_view
+from django.http import HttpResponse
+from rest_framework.filters import SearchFilter
+from rest_framework.response import Response
+from rest_framework.viewsets import ModelViewSet
+
+from .models import Product, Stock
+from .serializers import ProductSerializer, StockSerializer
+
+
+def index(request):
+    return HttpResponse('Hello cicd v2')
+
+
+class ProductViewSet(ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    # при необходимости добавьте параметры фильтрации
+    filter_backends = [SearchFilter]
+    search_fields = ['title', 'description']
+
+
+class StockViewSet(ModelViewSet):
+    queryset = Stock.objects.all()
+    serializer_class = StockSerializer
+    # при необходимости добавьте параметры фильтрации
+    filter_backends = [SearchFilter]
+    search_fields = ['products__title', 'products__description']
+
+
+@api_view(['GET'])
+def sample_view(request):
+    return Response({'message': 'Hello Students, Deploy Success'})
