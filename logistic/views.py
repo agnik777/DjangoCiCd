a897b194845ae265e6a950df3a273cd08a7c96e6
Andrey Nikolaev <agnik@europe.com>
2026-02-01 16:04:49 +0300

    CI-CD_Deploy

--- logistic/views.py
+++ logistic/views.py
@@ -31,4 +31,4 @@ class StockViewSet(ModelViewSet):
 
 @api_view(['GET'])
 def sample_view(request):
-    return Response({'message': 'Hello World!!! Deploy Success!!!'})
+    return Response({'message': 'Hello! CI-CD Deploy Success!!!'})
